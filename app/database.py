c5d16ec0647ca06cac80c44da95d416d398ab378
Oleksandr Korienev <alexkorienev@gmail.com>
2023-07-29 10:35:21 +0300

    Tweak database connection, try #2

--- app/database.py
+++ app/database.py
@@ -1,13 +1,28 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
+from furl import furl
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 from app.settings import settings
 
+
+postgres_url = furl(settings.DATABASE_URL.unicode_string())
+connect_args = {'timeout': 20}
+
+# Digitalocean does not allow us to specify driver for the sting
+if postgres_url.scheme == 'postgresql':
+    postgres_url.scheme = 'postgresql+asyncpg'
+
+# https://github.com/sqlalchemy/sqlalchemy/issues/6275
+for key, value in postgres_url.query.params.items():
+    connect_args[key] = value
+
+
 async_engine = create_async_engine(
-    settings.DATABASE_URL.unicode_string(),
+    postgres_url.url,
     future=True,
+    connect_args=connect_args,
 )
 async_session = async_sessionmaker(
     bind=async_engine,
--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import PostgresDsn, field_validator
-from furl import furl
+from pydantic import PostgresDsn
 
 
 class Settings(BaseSettings):
@@ -8,17 +7,6 @@ class Settings(BaseSettings):
 
     ENVIRONMENT: str
     DATABASE_URL: PostgresDsn
-
-    @field_validator("DATABASE_URL")
-    @classmethod
-    def process_url(cls, url: PostgresDsn):
-        """Digitalocean does not allow us to customize driver in the url string and it default to psycopg2"""
-        parts = furl(url.unicode_string())
-        if parts.scheme == 'postgresql':
-            parts.scheme = 'postgresql+asyncpg'
-
-        new_url_string = parts.url
-        return PostgresDsn(new_url_string)
 
 
 settings = Settings(_env_file='.env')
