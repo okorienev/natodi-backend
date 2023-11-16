80c738626cf73d12f79f9e6c791acfbcb8eb7c74
Oleksandr Korienev <alexkorienev@gmail.com>
2023-11-16 19:12:43 +0200

    Add pytest

---
+++ test/functional/api/test_health.py
@@ -0,0 +1,11 @@
+import pytest
+
+from httpx import AsyncClient
+
+@pytest.mark.asyncio
+async def test_health_ok(client: AsyncClient):
+    res = await client.get("/health")
+    res.raise_for_status()
+
+    data = res.json()
+    assert data["status"] == "ok"
